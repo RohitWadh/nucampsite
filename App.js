242c070966fc65ab528fe2f73521012c33cb7ba4
RohitWadh <toraktech@gmail.com>
2024-08-03 17:07:26 -0600

    Add all changes

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
-
-return (
-  <NavigationContainer>
-    <Main />
-  </NavigationContainer>
-   ); 
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Main />
+            </NavigationContainer>
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}); 
